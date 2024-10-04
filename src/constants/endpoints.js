2dfe57479c49bf0db95eedde551d420f7677a27b
shau8122 <shaukatali28122001@gmail.com>
2024-10-04 17:01:30 +0530

    url cahnges

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,6 +1,6 @@
 export const BASE = "https://squid-app-5tnzr.ondigitalocean.app";
 // export const BASE = "http://localhost:5000";
-export const BASEML = "http://localhost:8000";
+export const BASEML = "https://merry-blindly-stud.ngrok-free.app";
 // export const BASEML = "https://05f9-203-110-242-13.ngrok-free.app";
 // export const BASE = "https://ruralclassroom.onrender.com";
 // file
